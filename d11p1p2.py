5a38de09c8830be5cbbb49d9f9d2486807c04379
Aarni Koskela <akx@iki.fi>
2018-12-11 14:12:02 +0200

    Day 11, part 2 (fast, with Numba!)

--- d11p1p2.py
+++ d11p1p2.py
@@ -1,6 +1,8 @@
-import collections
+# This really should be optimized to use the previously calculated
+# rectangles, but since it only takes 2 minutes with Numba end-to-end
+# without that, I don't much care :)
 
-from tqdm import tqdm
+from numba import jit, prange
 
 
 def get_fuel(gsn, x, y):
@@ -11,45 +13,61 @@ def get_fuel(gsn, x, y):
     return hdig - 5
 
 
-def calc_top_fuel(grid, top_fuel, *, width, height, sq_size):
-    sq_l = list(range(sq_size))
-    for y in range(width - sq_size):
-        for x in range(height - sq_size):
-            for dy in sq_l:
-                for dx in sq_l:
+@jit(nopython=True, parallel=True)
+def calc_top_fuel(grid, *, width, height, sq_size):
+    top_width = width - sq_size
+    top_height = height - sq_size
+    top_fuel = [0] * ((top_width) * (top_height))
+    for y in prange(top_height):
+        for x in prange(top_width):
+            for dy in prange(sq_size):
+                for dx in prange(sq_size):
                     sx = x + dx
                     sy = y + dy
-                    top_fuel[x + 1, y + 1] += grid[sy * width + sx]
+                    topf_off = (y * top_width + x)
+                    top_fuel[topf_off] += grid[sy * width + sx]
+    return top_fuel
 
 
 def gen_grid(gsn, width, height):
     grid = [0] * (width * height)
-    for y in range(width):
-        for x in range(height):
+    for y in range(height):
+        for x in range(width):
             grid[y * width + x] = get_fuel(gsn, x, y)
     return grid
+
+
+def get_best_coords_1based(top_fuel, width, height):
+    best_x = best_y = 0
+    best_score = None
+    for y in range(height):
+        for x in range(width):
+            score = top_fuel[y * width + x]
+            if best_score is None or score > best_score:
+                best_x = x
+                best_y = y
+                best_score = score
+    return ((best_x + 1, best_y + 1), best_score)
 
 
 def part1(gsn, sq_size=3):
     width = height = 300
     grid = gen_grid(gsn, width, height)
-    top_fuel = collections.Counter()
-    calc_top_fuel(grid, top_fuel, width=width, height=height, sq_size=sq_size)
-    return top_fuel.most_common(1)[0]
+    top_fuel = calc_top_fuel(grid, width=width, height=height, sq_size=sq_size)
+    return get_best_coords_1based(top_fuel, width=(width - sq_size), height=(height - sq_size))
 
 
 def part2(gsn):
     width = height = 300
     grid = gen_grid(gsn, width, height)
-    top_fuel = collections.defaultdict(collections.Counter)
+    top_fuel = {}
+    best = {}
     for sq_size in range(1, min(width, height)):
         print(sq_size)
-        calc_top_fuel(grid, top_fuel[sq_size], width=width, height=height, sq_size=sq_size)
-    best_top_fuel = collections.Counter()
-    for sq_size, fuels in top_fuel.items():
-        for (x, y), fuel in fuels.items():
-            best_top_fuel[x, y, sq_size] += fuel
-    return best_top_fuel.most_common(1)[0]
+        top_fuel[sq_size] = calc_top_fuel(grid, width=width, height=height, sq_size=sq_size)
+        best[sq_size] = get_best_coords_1based(top_fuel[sq_size], width=(width - sq_size), height=(height - sq_size))
+    print(best)
+    print(sorted(best.items(), key=lambda p: p[1][1]))
 
 
 def selfcheck():
